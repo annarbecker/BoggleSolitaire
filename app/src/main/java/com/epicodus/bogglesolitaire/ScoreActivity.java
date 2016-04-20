eeda6b446d26c8795d3a4cc9b9582d57533e70a7
Anna Becker and Michelle Brecunier <anna.r.becker@gmail.com>
2016-04-20 15:54:41 -0700

    display list of words on score activity

--- app/src/main/java/com/epicodus/bogglesolitaire/ScoreActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/ScoreActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.bogglesolitaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ScoreActivity extends AppCompatActivity {
+    @Bind(R.id.userListView) ListView mUserListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String userWordsString = intent.getStringExtra("wordArray");
+        String[] userWordsArray = userWordsString.split(", ");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, userWordsArray);
+        mUserListView.setAdapter(adapter);
     }
 }
