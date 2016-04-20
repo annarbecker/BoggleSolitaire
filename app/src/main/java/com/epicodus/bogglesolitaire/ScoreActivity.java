61ac4257610336d4bb83153f85fb65f52f65d45c
Anna Becker and Michelle Brecunier <anna.r.becker@gmail.com>
2016-04-20 16:23:01 -0700

    separate invalid words and scoring words on the score page

--- app/src/main/java/com/epicodus/bogglesolitaire/ScoreActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/ScoreActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ScoreActivity extends AppCompatActivity {
-    @Bind(R.id.userListView) ListView mUserListView;
+    @Bind(R.id.invalidListView) ListView mInvalidListView;
+    @Bind(R.id.scoringListView) ListView mScoringListView;
 
 
     @Override
@@ -22,8 +26,22 @@ public class ScoreActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userWordsString = intent.getStringExtra("wordArray");
         String[] userWordsArray = userWordsString.split(", ");
+        List<String> invalidWordsArray = new ArrayList<String>();
+        List<String> scoringWordsArray = new ArrayList<String>();
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, userWordsArray);
-        mUserListView.setAdapter(adapter);
+
+        for (String word: userWordsArray) {
+            if (word.length() < 3) {
+                invalidWordsArray.add(word);
+            } else {
+                scoringWordsArray.add(word);
+            }
+        }
+
+
+        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, invalidWordsArray);
+        mInvalidListView.setAdapter(adapter1);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, scoringWordsArray);
+        mScoringListView.setAdapter(adapter2);
     }
 }
